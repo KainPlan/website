3852f4f6c9755ff69eb9a10a6bf2094d5191b275
MattMoony <monschein.matthias@gmail.com>
2020-01-26 23:11:58 +0100

    cleaning up; transitioning to nextjs

--- sass-watch.ps1
+++
@@ -1,1 +0,0 @@
-sass --watch res/scss:public/style --no-source-map
--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const error = require('./error');
-
-exports.restrict = function (req, res, next) {
-  if (req.session.uname) {
-    next();
-  } else {
-    error.err_msg(res, 403);
-  }
-};
