8e129763ebcb8bb9dd96d12f6f5a3030c14cf328
MattMoony <monschein.matthias@gmail.com>
2020-02-05 18:34:37 +0100

    minor changes

--- components/kainplan/Map.tsx
+++ components/kainplan/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactComponentElement } from 'react';
 import KPMap from '../../lib/models/KPMap';
 
 interface MapProps {
@@ -19,6 +19,7 @@ interface MapState {
   can: React.RefObject<HTMLCanvasElement>;
   ctx: CanvasRenderingContext2D;
   map: KPMap;
+  background: HTMLImageElement[];
   clockRate: number;
   animTime: number;
   animIntval: number;
@@ -35,6 +36,8 @@ class Map extends React.Component<MapProps, MapState> {
       can: React.createRef(),
       ctx: null,
       map: props.map,
+      background: new Array(props.map.background.length)
+                      .fill(null),
       clockRate: props.clockRate || 10,
       animTime: props.animTime || 100,
       animIntval: props.animIntval || null,
@@ -48,6 +51,14 @@ class Map extends React.Component<MapProps, MapState> {
     }
     this.setState({ 
       ctx: this.state.can.current.getContext('2d'),
+      background: this.state.map.background
+                  .map(b => {
+                    let im = new Image();
+                    im.src = b;
+                    return im;
+                  }),
+    }, () => {
+      this.onSwitchFloor(0);
     });
   }
 
@@ -58,10 +69,60 @@ class Map extends React.Component<MapProps, MapState> {
     });
   }
 
+  onMouseDown() {
+
+  }
+
+  onMouseMove() {
+
+  }
+
+  onMouseZoom() {
+
+  }
+
+  onMouseUp() {
+
+  }
+
+  onTouchDown() {
+
+  }
+
+  onTouchMove() {
+
+  }
+
+  onTouchUp() {
+
+  }
+
+  onSwitchFloor(fid) {
+    this.state.ctx.drawImage(this.state.background[fid], 
+                              0, 0, 
+                              this.state.map.width, this.state.map.height);
+  }
+
   render() {
     return (
       <>
-        <canvas ref={this.state.can} width={this.state.width} height={this.state.height}></canvas>
+        <canvas 
+          ref={this.state.can} 
+          width={this.state.width} 
+          height={this.state.height}
+          onWheel={this.onMouseZoom.bind(this)}
+          onMouseDown={this.onMouseDown.bind(this)}
+          onMouseMove={this.onMouseMove.bind(this)}
+          onMouseUp={this.onMouseUp.bind(this)}
+          onMouseLeave={this.onMouseUp.bind(this)}
+          onMouseOut={this.onMouseUp.bind(this)}
+          onPointerDown={this.onTouchDown.bind(this)}
+          onPointerMove={this.onTouchMove.bind(this)}
+          onPointerUp={this.onTouchUp.bind(this)}
+          onPointerCancel={this.onTouchUp.bind(this)}
+          onPointerOut={this.onTouchUp.bind(this)}
+          onPointerLeave={this.onTouchUp.bind(this)}  
+        ></canvas>
         <div>
           {this.state.children}
         </div>
@@ -74,7 +135,7 @@ class Map extends React.Component<MapProps, MapState> {
               width: ${this.state.width}px;
               height: ${this.state.height}px;
             `}
-            background-color: lightblue;
+            background-color: #DFF8FF;
             position: relative;
           }
         `}</style>
